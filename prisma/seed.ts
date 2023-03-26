b365b0ccf5bd5faa150549888fe462b0d6edf553
Jake Waldron <jakewaldron@gmail.com>
2023-03-26 15:29:48 -0400

    seed products and categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,77 +1,92 @@
 // LEAVING THIS AS AN EXAMPLE
 
-// import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import fs from "fs";
+import Papa from "papaparse";
 
 // const prisma = new PrismaClient();
+import { prisma } from "../src/lib/prisma";
 
-// const userData = [
-//   {
-//     name: "Alice",
-//     email: "alice@prisma.io",
-//     username: "alice123",
-//     posts: {
-//       create: [
-//         {
-//           title: "Join the Prisma Slack",
-//           content: "https://slack.prisma.io",
-//           published: true,
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     name: "Nilu",
-//     email: "nilu@prisma.io",
-//     username: "nilu123",
-//     posts: {
-//       create: [
-//         {
-//           title: "Follow Prisma on Twitter",
-//           content: "https://www.twitter.com/prisma",
-//           published: true,
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     name: "Mahmoud",
-//     email: "mahmoud@prisma.io",
-//     username: "mahmoud123",
-//     posts: {
-//       create: [
-//         {
-//           title: "Ask a question about Prisma on GitHub",
-//           content: "https://www.github.com/prisma/prisma/discussions",
-//           published: true,
-//         },
-//         {
-//           title: "Prisma on YouTube",
-//           content: "https://pris.ly/youtube",
-//         },
-//       ],
-//     },
-//   },
-// ] satisfies Prisma.UserCreateInput[];
+const productPath = "./prisma/data/products_raw.data.csv";
+const productFile = fs.readFileSync(productPath, "utf8");
+const categoryPath = "./prisma/data/categories.data.csv";
+const categoryFile = fs.readFileSync(categoryPath, "utf8");
+const coreProductsPath = "./prisma/data/core_products.data.csv";
+const coreProductsFile = fs.readFileSync(coreProductsPath, "utf8");
 
-// async function main() {
-//   console.log(`Start seeding ...`);
-//   for (const u of userData) {
-//     const user = await prisma.user.create({
-//       data: u,
-//     });
-//     console.log(`Created user with id: ${user.id}`);
-//   }
-//   console.log(`Seeding finished.`);
-// }
+function seedFromFiles() {
+  // ------- SEED CATEGORY TABLE -------
+  Papa.parse(categoryFile, {
+    header: false, // Set to true if the CSV file has a header row
+    dynamicTyping: true, // Automatically convert values to their appropriate data types
+    complete: function (results) {
+      const data = results.data;
+      data.forEach(async (line: any) => {
+        const [categoryId, months] = line;
+        await prisma.category.create({
+          data: {
+            categoryId,
+            months,
+          },
+        });
+      });
+    },
+  });
 
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
+  // get core products
+  let coreProducts: any[] = [];
+  Papa.parse(coreProductsFile, {
+    header: true,
+    // dynamicTyping: true,
+    complete: (results) => {
+      coreProducts = results.data;
+    },
+  });
 
-export {};
+  // ------- SEED PRODUCT TABLE -------
+  Papa.parse(productFile, {
+    header: true,
+    dynamicTyping: true,
+    complete: async (results) => {
+      const data = results.data;
+      const uploadData = data.map((line: any) => {
+        if (line.vendor !== "SMOOTH-ON") return;
+        const { partNum, name, categoryId, weight, productPK } = line;
+        const searchName = name?.replace(/ \((?!.*\().*?\)/, "") || name;
+        const isCoreProduct = coreProducts.find(
+          (coreProduct) => coreProduct.partNum.toString() === partNum.toString()
+        );
+        return {
+          partNum: partNum.toString(),
+          name,
+          categoryId,
+          weight,
+          coreProduct: isCoreProduct ? true : false,
+          productPK,
+          searchName,
+        };
+      });
+      await prisma.product.createMany({
+        data: uploadData,
+      } as Prisma.ProductCreateManyArgs);
+    },
+  });
+}
+
+// // this is where it takes the data and uploads it
+async function main() {
+  console.log(`Start seeding ...`);
+  seedFromFiles();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+// export {};
