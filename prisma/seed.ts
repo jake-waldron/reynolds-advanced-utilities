e337c93cf0b918d02fe72dbf87209087e7deaebd
Jake Waldron <jakewaldron@gmail.com>
2023-03-26 16:15:29 -0400

    added types

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,23 @@
-// LEAVING THIS AS AN EXAMPLE
-
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Category, Prisma, Product } from "@prisma/client";
 import fs from "fs";
 import Papa from "papaparse";
 
-// const prisma = new PrismaClient();
 import { prisma } from "../src/lib/prisma";
+
+type CoreProductCsvData = {
+  partNum: string;
+  name: string;
+  productPK: number;
+};
+
+type ProductCsvData = {
+  partNum: string;
+  name: string;
+  vendor: string;
+  categoryId: string;
+  weight: string;
+  productPK: string;
+};
 
 const productPath = "./prisma/data/products_raw.data.csv";
 const productFile = fs.readFileSync(productPath, "utf8");
@@ -17,12 +29,12 @@ const coreProductsFile = fs.readFileSync(coreProductsPath, "utf8");
 function seedFromFiles() {
   // ------- SEED CATEGORY TABLE -------
   Papa.parse(categoryFile, {
-    header: false, // Set to true if the CSV file has a header row
+    header: true, // Set to true if the CSV file has a header row
     dynamicTyping: true, // Automatically convert values to their appropriate data types
     complete: function (results) {
-      const data = results.data;
-      data.forEach(async (line: any) => {
-        const [categoryId, months] = line;
+      const data = results.data as Array<Category>;
+      data.forEach(async (line) => {
+        const { categoryId, months } = line;
         await prisma.category.create({
           data: {
             categoryId,
@@ -34,12 +46,12 @@ function seedFromFiles() {
   });
 
   // get core products
-  let coreProducts: any[] = [];
+  let coreProducts: CoreProductCsvData[];
   Papa.parse(coreProductsFile, {
     header: true,
     // dynamicTyping: true,
     complete: (results) => {
-      coreProducts = results.data;
+      coreProducts = results.data as CoreProductCsvData[];
     },
   });
 
@@ -48,24 +60,25 @@ function seedFromFiles() {
     header: true,
     dynamicTyping: false,
     complete: async (results) => {
-      const data = results.data;
-      const uploadData = data.map((line: any) => {
-        if (line.vendor !== "SMOOTH-ON") return;
-        const { partNum, name, categoryId, weight, productPK } = line;
-        const searchName = name?.replace(/ \((?!.*\().*?\)/, "") || name;
-        const isCoreProduct = coreProducts.find(
-          (coreProduct) => coreProduct.partNum === partNum
-        );
-        return {
-          partNum: partNum,
-          name,
-          categoryId: categoryId || "MISCELLANEOUS",
-          weight: parseFloat(weight),
-          coreProduct: isCoreProduct ? true : false,
-          productPK: parseInt(productPK),
-          searchName,
-        };
-      });
+      const data = results.data as ProductCsvData[];
+      const uploadData = data
+        .filter((product) => product.vendor === "SMOOTH-ON")
+        .map((line): Product => {
+          const { partNum, name, categoryId, weight, productPK } = line;
+          const searchName = name?.replace(/ \((?!.*\().*?\)/, "") || name;
+          const isCoreProduct = coreProducts.find(
+            (coreProduct) => coreProduct.partNum === partNum
+          );
+          return {
+            partNum: partNum,
+            name,
+            categoryId: categoryId || "MISCELLANEOUS",
+            weight: parseFloat(weight),
+            coreProduct: isCoreProduct ? true : false,
+            productPK: parseInt(productPK),
+            searchName,
+          };
+        });
       await prisma.product.createMany({
         data: uploadData,
       } as Prisma.ProductCreateManyArgs);
