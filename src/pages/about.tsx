25718d2a445f705345b22df04f8e9809646c9dd0
Jake Waldron <jakewaldron@gmail.com>
2023-03-26 16:34:00 -0400

    remove pages

--- src/pages/about.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <main className="flex h-screen w-full flex-col items-center justify-center">
-      <h1 className="text-3xl">This is The About Page</h1>
-    </main>
-  );
-}
