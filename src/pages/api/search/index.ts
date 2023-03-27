01be6568ae4aac8b15f27c05038d7bc7daeb127c
Jake Waldron <jakewaldron@gmail.com>
2023-03-27 10:03:02 -0400

    update error response

--- src/pages/api/search/index.ts
+++ src/pages/api/search/index.ts
@@ -36,10 +36,10 @@ export default async function handler(
   await runMiddleware(req, res, cors);
 
   const { q } = req.query;
-  if (!q) return res.status(400).json({ error: "No query provided" });
+  // if (!q) return res.status(400).json({ data: { error: "No query provided" } });
 
   if (typeof q !== "string")
-    return res.status(400).json({ error: "Query must be a string" });
+    return res.status(400).json({ data: { error: "Query must be a string" } });
 
   // send products to fuse to filter out based on query
   const products = await getResults(q);
