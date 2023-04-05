55e835e0622a18505dfc6d9f403cb5648dc1d2d4
Jake Waldron <jakewaldron@gmail.com>
2023-04-04 22:16:14 -0400

    refactor sorting method

--- src/lib/api/search/sort.ts
+++ src/lib/api/search/sort.ts
@@ -1,53 +1,50 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 type Product = {
   name: string;
   score: number;
   partNum: string;
 };
 
-function sort(arrayToSort) {
-  const scores = arrayToSort.map((product) => product.score);
+function sort(arrayToSort: Product[]): Product[][] {
+  const scores = arrayToSort.map((product: Product) => product.score);
 
   const scoreSet = new Set(scores);
 
-  const groupedByScore = [...scoreSet].map((score) => {
-    return arrayToSort.filter((product) => product.score === score);
+  const groupedByScore = [...scoreSet].map((score: number) => {
+    return arrayToSort.filter((product: Product) => product.score === score);
   });
 
-  const sortedArray = groupedByScore.map((group) => sortArray(group));
+  const sortedArray = groupedByScore.map((group: Product[]) =>
+    sortArray(group)
+  );
   return sortedArray;
 }
 
-function sortArray(arr): Product[] {
-  const order = {
-    "2-OZ": 0,
-    "4-OZ": 1,
-    PINT: 2,
-    TRIAL: 3,
-    "1-GAL": 4,
-    "5-GAL": 5,
-    "5-GAL PART": 99,
-    UNKNOWN: 6,
-  };
+const sizeOrder: string[] = [
+  "2-OZ",
+  "4-OZ",
+  "PINT",
+  "TRIAL",
+  "1-GAL",
+  "5-GAL",
+  "UNKNOWN",
+  "PART",
+];
 
-  const newOrder = {};
-  arr.forEach((entry: Product) => {
-    const sizeRegex = /\b(2-OZ|4-OZ|PINT|TRIAL|1-GAL|5-GAL|5-GAL PART)\b/;
-    const size = entry.name.match(sizeRegex);
-    const sizeName = size ? size[1] : "UNKNOWN";
-    let orderIndex = order[sizeName];
-    if (newOrder[orderIndex]) {
-      orderIndex = parseInt(Object.keys(newOrder).at(-1)) + 1;
-    }
-    newOrder[orderIndex] = entry;
-  });
-  return Object.keys(newOrder)
-    .sort((a, b) => +a - +b)
-    .map((key) => newOrder[key]);
+function getSize(product: Product): string {
+  if (product.name.includes("PART")) return "PART";
+  const sizeRegex = /\b(2-OZ|4-OZ|PINT|TRIAL|1-GAL|5-GAL)\b/;
+  const size = product.name.match(sizeRegex);
+  return size ? size[1] : "UNKNOWN";
 }
 
-export function sortBySize(arrayToSort): Product[] {
+function sortArray(arr: Product[]): Product[] {
+  return arr.sort((p1, p2) => {
+    const size1 = getSize(p1);
+    const size2 = getSize(p2);
+    return sizeOrder.indexOf(size1) - sizeOrder.indexOf(size2);
+  });
+}
+
+export function sortBySize(arrayToSort: Product[]): Product[] {
   return sort(arrayToSort).flat();
 }
